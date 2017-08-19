05553389142652559fa2611beab5d9791a9d3ac3
Luca Stefani <luca020400@lineageos.org>
2017-08-19 12:10:27 +0200

    wt88047: gps: Add get_process_name shim
    Change-Id: I10ce654b14866d41610c7e4098df9691b983d515

--- board/shims.mk
+++ board/shims.mk
@@ -1,5 +1,6 @@
 # Shims
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/bin/mm-qcamera-daemon|libshim_camera.so \
+    /system/vendor/lib/libizat_core.so|libshims_get_process_name.so \
     /system/vendor/lib/libmmcamera2_imglib_modules.so|libshim_camera.so \
     /system/vendor/lib/lib-imsvt.so|libshims_ims.so
--- libshims/Android.mk
+++ libshims/Android.mk
@@ -31,3 +31,13 @@ LOCAL_SHARED_LIBRARIES := libgui libutils liblog libsensor libbinder libandroid
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    get_process_name/get_process_name.c
+
+LOCAL_MODULE := libshims_get_process_name
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
