1c43caa2c5d0ae78a382c0a8c21afc1fd7eedf98
karthikkm007 <jacksparrow941994@gmail.com>
2018-05-07 21:55:42 +0530

    wt88047: proper vendor path set

--- device.mk
+++ device.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(TARGET_COPY_OUT_VENDOR)/wingtech/wt88047/wt88047-vendor.mk)
-$(call inherit-product, $(TARGET_COPY_OUT_VENDOR)/wingtech/volte/volte.mk)
-$(call inherit-product, $(TARGET_COPY_OUT_VENDOR)/wingtech/volte/wt88047/wt88047-vendor.mk)
+$(call inherit-product, vendor/wingtech/wt88047/wt88047-vendor.mk)
+$(call inherit-product, vendor/wingtech/volte/volte.mk)
+$(call inherit-product, vendor/wingtech/volte/wt88047/wt88047-vendor.mk)
 
 # ANT+
 PRODUCT_PACKAGES += \
