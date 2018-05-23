4b4dc2b1d1068714a79302a48fd961bc85a56dd2
MkfsSion <myychina28759@gmail.com>
2018-05-23 17:35:06 +0800

    wt88047: Update manifest and compatibility_matrix.
    * Bring up binderized graphics composer
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- device.mk
+++ device.mk
@@ -137,6 +137,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
@@ -357,10 +359,6 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
-
-# HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
 
 # Init scripts
 PRODUCT_PACKAGES += \
