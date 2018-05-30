898a40ea2c1aaef5ff17c563e19b4ba00b2d3e7d
karthikkm007 <jacksparrow941994@gmail.com>
2018-05-30 21:33:41 +0530

    wt88047: we don't need to shim now
    As its now in vendor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -191,12 +191,12 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 #include device/qcom/sepolicy/legacy-sepolicy.mk
 
 # Shims
-TARGET_LD_SHIM_LIBS := \
-    /vendor/bin/mm-qcamera-daemon|libshim_camera.so \
-    /vendor/lib/libflp.so|libshims_flp.so \
-    /vendor/lib/libizat_core.so|libshims_get_process_name.so \
-    /vendor/lib/libmmcamera2_imglib_modules.so|libshim_camera.so \
-    /vendor/lib/lib-imsvt.so|libshims_ims.so
+#TARGET_LD_SHIM_LIBS := \
+#    /vendor/bin/mm-qcamera-daemon|libshim_camera.so \
+#    /vendor/lib/libflp.so|libshims_flp.so \
+#    /vendor/lib/libizat_core.so|libshims_get_process_name.so \
+#    /vendor/lib/libmmcamera2_imglib_modules.so|libshim_camera.so \
+#    /vendor/lib/lib-imsvt.so|libshims_ims.so
 
 # Wlan
 BOARD_HAS_QCOM_WLAN := true
