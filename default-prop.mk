fa26e2bb721e72a0fb785b1e6740e53915600b96
karthikkm007 <jacksparrow941994@gmail.com>
2018-06-01 16:52:03 +0530

    wt88047: enable adb on boot

--- default-prop.mk
+++ default-prop.mk
@@ -12,7 +12,4 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Debug
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.adb.secure=0 \
-    ro.secure=0 \
-    persist.sys.usb.config=adb,mtp \
-    ro.debugable=1
+    ro.adb.secure=0 
