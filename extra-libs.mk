6fd057a324019ab78ce5af788308a727c044a872
MkfsSion <myychina28759@gmail.com>
2018-06-01 19:52:06 +0800

    wt88047: Move all extra libraries to separate makefile.
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

---
+++ extra-libs.mk
@@ -0,0 +1,8 @@
+# Camera
+PRODUCT_ACKAGES += \
+   android.hardware.camera.device@1.0.vendor \
+   android.hardware.camera.common@1.0.vendor
+
+# Sensor
+PRODUCT_PACKAGES += \
+   android.hardware.sensors@1.0.vendor
