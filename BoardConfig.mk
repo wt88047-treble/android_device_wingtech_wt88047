1b181748a84e1dcf44444d8aefe27c50c38189e8
karthikkm007 <jacksparrow941994@gmail.com>
2018-06-04 19:58:58 +0530

    kernel now prebuilt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,9 +117,10 @@ TARGET_LIBINIT_MSM8916_DEFINES_FILE := $(DEVICE_PATH)/init/init_wt88047.cpp
 # Kernel
 BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_KERNEL_SEPARATED_DT := true
-TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
-TARGET_KERNEL_CONFIG := lineageos_wt88047_defconfig
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
+#BOARD_KERNEL_SEPARATED_DT := true
+#TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
+#TARGET_KERNEL_CONFIG := lineageos_wt88047_defconfig
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1 console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
