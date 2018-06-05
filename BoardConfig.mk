c4027945ec53d0ad4bbaaf8444048eaf34037fce
karthikkm007 <jacksparrow941994@gmail.com>
2018-06-05 14:26:49 +0530

    now we don't need this

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,14 +182,6 @@ BOARD_SEPOLICY_DIRS += \
 #include device/qcom/sepolicy/sepolicy.mk
 #include device/qcom/sepolicy/legacy-sepolicy.mk
 
-# Shims
-#TARGET_LD_SHIM_LIBS := \
-#    /vendor/bin/mm-qcamera-daemon|libshim_camera.so \
-#    /vendor/lib/libflp.so|libshims_flp.so \
-#    /vendor/lib/libizat_core.so|libshims_get_process_name.so \
-#    /vendor/lib/libmmcamera2_imglib_modules.so|libshim_camera.so \
-#    /vendor/lib/lib-imsvt.so|libshims_ims.so
-
 # Wlan
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HOSTAPD_DRIVER := NL80211
