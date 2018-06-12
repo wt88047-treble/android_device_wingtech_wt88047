596cc360ed73e56674013ce726ececc72e30140f
MkfsSion <2875935638@qq.com>
2018-06-12 19:57:26 +0800

    wt88047: light: Bring up binderized light HAL.
    Signed-off-by: MkfsSion <2875935638@qq.com>
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- device.mk
+++ device.mk
@@ -263,7 +263,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.aw2003
+    android.hardware.light@2.0-service.wt88047
 
 # Media
 PRODUCT_COPY_FILES += \
