09b0340ea7f88fbd5f88a52bb582674bd82bf849
MkfsSion <myychina28759@gmail.com>
2018-06-25 16:52:52 +0800

    wt88047: Build our binderized camera provider HAL.
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- device.mk
+++ device.mk
@@ -289,7 +289,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service.wt88047 \
     camera.device@3.2-impl \
     camera.msm8916 \
     libmm-qcamera \
