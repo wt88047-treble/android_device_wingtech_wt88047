f7ebecc095c5c7457d0f1d6d9c877985cd9d9c2a
MkfsSion <myychina28759@gmail.com>
2018-07-10 15:43:13 +0800

    wt88047: Use aw2013 common light HAL.
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- device.mk
+++ device.mk
@@ -259,7 +259,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.wt88047
+    android.hardware.light@2.0-service.aw2013
 
 # Media
 PRODUCT_COPY_FILES += \
