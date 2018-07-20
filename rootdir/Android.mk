53959536f790c010e390df097ed0ad8d465a4362
MkfsSion <myychina28759@gmail.com>
2018-07-20 20:30:14 +0800

    wt88047: rootdir: Symlink acdbdata to /system/etc on boot trigger.
    * Our acdbloader is hardcoded to load acdb files from /etc
    
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -10,6 +10,14 @@ LOCAL_SRC_FILES    := bin/init.qcom.mem.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.acdb.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.acdb.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
 # Init scripts
 
 include $(CLEAR_VARS)
