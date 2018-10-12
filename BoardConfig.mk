749a4ed082bb443e36227b67bee072b42a9ec8ed
karthikkm007 <jacksparrow941994@gmail.com>
2018-10-12 12:30:29 +0530

    wt88047: deodex system probably straight way
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,14 +196,7 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Deodex system ** will have loonger boot time ** sed AF
-ifeq ($(HOST_OS),linux)
-    ifneq ($(TARGET_BUILD_VARIANT),eng)
-        ifeq ($(WITH_DEXPREOPT),)
-            WITH_DEXPREOPT := false
-        endif
-    endif
-endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+WITH_DEXPREOPT := false
 
 # Vendor
 include vendor/wingtech/wt88047/BoardConfigVendor.mk
