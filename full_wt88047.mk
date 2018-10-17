ee82e88847b16fc18a71665c1643fd4fe60f0abb
cecell3000 <cecell3000@gmail.com>
2018-10-17 00:44:03 -0300

    wt88047: re-order full_wt88047.mk

--- full_wt88047.mk
+++ full_wt88047.mk
@@ -15,8 +15,8 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from wt88047 device
 $(call inherit-product, device/wingtech/wt88047/device.mk)
