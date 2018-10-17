a57548c49319b1915f01c18b84916341a8c27c85
cecell3000 <cecell3000@gmail.com>
2018-10-17 00:42:32 -0300

    wt88047: remove treble_common_64.mk from full_wt88047.mk

--- full_wt88047.mk
+++ full_wt88047.mk
@@ -16,7 +16,6 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/treble_common_64.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from wt88047 device
